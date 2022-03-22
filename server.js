8529345facdeb2ace390e239d50741cc919a73ee
Monakhrsn <Monakhrsn@gmail.com>
2022-03-22 16:02:05 +0100

    Connet mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
 
+// Connect Database
+connectDB();
 app.get('/', (req, res) => res.send('API Runnning'));
 
 const PORT = process.env.PORT || 5000;
